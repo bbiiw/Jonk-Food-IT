d8f8bc84c2d5216c46e93465a774e79069065052
Wongwiwat Neamoon <wongwiwat.2004@gmail.com>
2023-09-16 22:24:13 +0700

    update frontend

---
+++ frontend/src/ButtonAdd.js
@@ -0,0 +1,28 @@
+// main.js
+
+// เริ่มต้นค่าปริมาณที่ 0
+let quantityValue = 0;
+
+// รับค่าของ element ที่มี ID เป็น "quantity"
+const quantityElement = document.getElementById("quantity");
+
+// สร้างอาร์เรย์ของปุ่ม
+const addButtonList = [
+    document.getElementById("addButton1"),
+    document.getElementById("addButton2"),
+    document.getElementById("addButton3"),
+    document.getElementById("addButton4"),
+    document.getElementById("addButton5"),
+    document.getElementById("addButton6"),
+    document.getElementById("addButton7"),
+    document.getElementById("addButton8")
+  ];
+
+addButtonList.forEach(function (button) {
+    button.addEventListener("click", function () {
+      // เพิ่มค่าปริมาณ
+      quantityValue++;
+      // แสดงค่าปริมาณใน element
+      quantityElement.textContent = quantityValue;
+    });
+  });
---
+++ frontend/src/ButtonCart.js
@@ -0,0 +1,28 @@
+// Cart.Html
+
+let quantityValue = 0; // Initialize the quantity value
+
+function updateQuantityDisplay(id, value) {
+  document.getElementById(id).textContent = value;
+}
+
+function incrementQuantity(id) {
+  let quantityValue = parseInt(document.getElementById(id).textContent);
+  quantityValue++;
+  updateQuantityDisplay(id, quantityValue);
+}
+
+function decrementQuantity(id) {
+  let quantityValue = parseInt(document.getElementById(id).textContent);
+  if (quantityValue > 0) {
+    quantityValue--;
+    updateQuantityDisplay(id, quantityValue);
+  }
+}
+
+// Attach event listeners to the buttons
+document.getElementById("decrementButton").addEventListener("click", decrementQuantity);
+document.getElementById("incrementButton").addEventListener("click", incrementQuantity);
+
+// Initialize the quantity display
+updateQuantityDisplay();
