47015c8d33870af79521363c415649dfd331915c
Earth123456789 <earth247god@gmail.com>
2023-10-11 13:45:45 +0700

    REPORT

--- frontend/Admin/Report.js
+++ frontend/Admin/Report.js
@@ -1,39 +1,55 @@
-// report.js
+const chartTypeSelect = document.getElementById('chartType');
+chartTypeSelect.addEventListener('change', () => {
+  const selectedChartType = chartTypeSelect.value;
+  updateChart(selectedChartType); // เรียกใช้ฟังก์ชัน updateChart
+});
 
-const ctx = document.getElementById('myChart');
-
-// สร้างข้อมูลสำหรับกราฟ
-const data = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-  datasets: [{
-      label: 'Sample Dataset',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
+// สร้างฟังก์ชันสำหรับอัปเดตกราฟ
+function updateChart(selectedChartType) {
+  // กำหนดข้อมูลและค่าต่าง ๆ ของกราฟตามประเภทที่เลือก
+  let newData, newConfig;
+  if (selectedChartType === 'bar') {
+    newData = {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: 'Sample Dataset',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
           'rgba(54, 162, 235, 0.2)',
           'rgba(255, 206, 86, 0.2)',
           'rgba(75, 192, 192, 0.2)',
           'rgba(153, 102, 255, 0.2)',
           'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
+        ],
+        borderColor: [
           'rgba(255, 99, 132, 1)',
           'rgba(54, 162, 235, 1)',
           'rgba(255, 206, 86, 1)',
           'rgba(75, 192, 192, 1)',
           'rgba(153, 102, 255, 1)',
           'rgba(255, 159, 64, 1)'
-      ],
-      borderWidth: 1
-  }]
-};
+        ],
+        borderWidth: 1
+      }]
+    };
+    newConfig = {
+      type: 'bar',
+      data: newData,
+      options: {}
+    };
+  } else if (selectedChartType === 'line') {
+    // สร้างข้อมูลและค่าต่าง ๆ สำหรับ Line Chart
+    // ...
+  } else if (selectedChartType === 'pie') {
+    // สร้างข้อมูลและค่าต่าง ๆ สำหรับ Pie Chart
+    // ...
+  }
 
-// กำหนดค่าต่าง ๆ ของกราฟ
-const config = {
-  type: 'bar',
-  data: data,
-  options: {}
-};
-
-// สร้างและแสดงกราฟใน Canvas
-const myChart = new Chart(ctx, config);
+  // สร้างและแสดงกราฟใหม่ใน Canvas
+  if (newConfig) {
+    myChart.destroy(); // ทำลายกราฟเดิม
+    const newChart = new Chart(ctx, newConfig);
+    myChart = newChart; // เก็บอ้างอิงกราฟใหม่
+  }
+}
