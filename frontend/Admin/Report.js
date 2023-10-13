dea62b1c5b402b116e7338bb731057efb284de02
Earth123456789 <earth247god@gmail.com>
2023-10-13 17:55:40 +0700

    change color bar

--- frontend/Admin/Report.js
+++ frontend/Admin/Report.js
@@ -16,7 +16,7 @@ function initializeChart(queues) {
             datasets: [{
                 label: 'จำนวนคิวอาหารรายสัปดาห์',
                 data: queueData, // ใช้ queueData โดยตรง
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
                 borderColor: 'rgb(255, 99, 132)',
                 borderWidth: 3,
             }],
@@ -48,13 +48,13 @@ function updateChart(chartType, queueData, dayData) {
                 label: 'จำนวนคิวอาหารรายสัปดาห์',
                 data: queueData, // ใช้ queueData โดยตรง
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
-                    'rgba(255, 205, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(201, 203, 207, 0.2)'
+                    'rgba(255, 99, 132, 0.5)',
+                    'rgba(255, 159, 64, 0.5)',
+                    'rgba(255, 205, 86, 0.5)',
+                    'rgba(75, 192, 192, 0.5)',
+                    'rgba(54, 162, 235, 0.5)',
+                    'rgba(153, 102, 255, 0.5)',
+                    'rgba(201, 203, 207, 0.5)'
                 ],
                 borderColor: [
                     'rgb(255, 99, 132)',
